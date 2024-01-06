dd1a4df9af9fb7443729383911e6f33ccc9ad968
Abhijeet <dutta.abhijeet@outlook.com>
2024-01-06 17:13:32 +0530

    Collections Creation Success created collections automatically from models directory when we start the node

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,11 +1,32 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const config = require('./config.js');
+const fs = require('fs');
+const path = require('path');
 
 const connectToDatabase = async () => {
     try {
         await mongoose.connect(config.mongoURI, config.options);
         console.log(`Connected to MongoDB in ${process.env.NODE_ENV} environment`);
+
+        // Load all model files from the 'models' folder
+        const modelsPath = path.join(__dirname, '../models');
+        const modelFiles = fs.readdirSync(modelsPath);
+
+        modelFiles.forEach((file) => {
+            if (file.endsWith('.js')) {
+                const model = require(path.join(modelsPath, file));
+                
+                // Check if the model has a 'createCollection' method
+                if (model.createCollection && typeof model.createCollection === 'function') {
+                    model.createCollection().then(function (collection) {
+                        console.log(`Collection for ${file} is created!`);
+                    });
+                } else {
+                    console.log(`Skipping ${file} as it does not have a 'createCollection' method.`);
+                }
+            }
+        });
     } catch (error) {
         console.log(`Error connecting to MongoDB: ${error}`);
     }
